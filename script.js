2cfeac4018bef6588aaa1f710ce60717060c79b9
Johne04 <johne1708@gmail.com>
2024-10-17 08:44:18 +0100

    Video home page outside clisk close video

---
+++ script.js
@@ -0,0 +1,15 @@
+const navLinks = document.querySelectorAll('.nav ul li a');
+
+const mainPic = document.getElementById('main-pic');
+const showVid = document.getElementById("showVideoBtn");
+const videoContainer = document.getElementById("videoContainer")
+
+navLinks.forEach((link, index) => {
+  link.classList.add('show');
+  link.style.transitionDelay = `${index * 0.1}s`;
+});
+
+
+showVid.addEventListener('click', () =>  videoContainer.style.display = "block"); 
+
+mainPic.addEventListener('click', () =>   videoContainer.style.display = "none"); 
