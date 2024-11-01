139b9f97ed239ae3f59079762dea87e1712caa22
Johne04 <johne1708@gmail.com>
2024-11-01 13:19:56 +0000

    FAQ's added

--- script.js
+++ script.js
@@ -47,3 +47,18 @@ nextButton.addEventListener("click", () => showSlide(currentIndex + 1));
 
 // Optional: Auto-slide
 setInterval(() => showSlide(currentIndex + 1), 5000);
+
+/*FAQ's section*/ 
+const navToggle = document.querySelector(
+  '[aria-controls="primary-navigation"]'
+);
+
+navToggle.addEventListener("click", () => {
+  const navOpened = navToggle.getAttribute("aria-expanded");
+
+  if (navOpened === "false") {
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
